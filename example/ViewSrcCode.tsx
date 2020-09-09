dc689c91b711ef507f8f793280899df1027c8e2c
Luke Helg <luke.helg@gmail.com>
2020-09-09 17:19:16 +0900

    remove examples

--- example/ViewSrcCode.tsx
+++ example/ViewSrcCode.tsx
@@ -1,17 +1,23 @@
-import * as React from "react";
-import { Component } from "react";
+import * as React from 'react';
+import { Component } from 'react';
 
 class ViewSrcCode extends Component<{ srcCodeUrl: string }, {}> {
-    constructor(props: Readonly<{ srcCodeUrl: string; }>) {
-        super(props);
-    }
+  constructor(props: Readonly<{ srcCodeUrl: string }>) {
+    super(props);
+  }
 
-    public render() {
-        const { srcCodeUrl } = this.props;
-        return (
-            <span style={{ marginLeft: "10px" }} className="help-text">(<a href={srcCodeUrl} target="_blank">&lt;/&gt;View example source code</a>)</span>
-        );
-    }
+  public render() {
+    const { srcCodeUrl } = this.props;
+    return (
+      <span style={{ marginLeft: '10px' }} className="help-text">
+        (
+        <a href={srcCodeUrl} target="_blank">
+          &lt;/&gt;View example source code
+        </a>
+        )
+      </span>
+    );
+  }
 }
 
 export default ViewSrcCode;
--- example/withDnDContext.ts
+++ example/withDnDContext.ts
@@ -1,3 +1,3 @@
-import {DragDropContext} from "react-dnd";
-import HTML5Backend from "react-dnd-html5-backend";
+import { DragDropContext } from 'react-dnd';
+import HTML5Backend from 'react-dnd-html5-backend';
 export default DragDropContext(HTML5Backend);
